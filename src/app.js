3b8bfb4aff1422493486d5a282b63aff227d0ac8
Egor Yudin <eydnep2000@gmail.com>
2020-10-15 18:18:20 +0300

    New dependencies were added.

--- src/app.js
+++ src/app.js
@@ -2,8 +2,19 @@ const express = require("express");
 const middleware = require("./middlewares/index");
 const routes = require("./routes/index");
 
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const corsOptions = {
+  origin: "http://localhost:8081"
+};
+
 const app = express();
 const PORT = process.env.PORT || 4000;
+
+app.use(cors(corsOptions));
+
+app.use(bodyParser.json());
 
 app.use("/", middleware);
 app.use("/", routes);
