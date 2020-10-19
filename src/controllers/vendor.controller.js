5bd74e07e979aa1f5b21d63e675555194ac99636
Egor Yudin <eydnep2000@gmail.com>
2020-10-19 21:53:07 +0300

    Vendor and category controllers were added.

---
+++ src/controllers/vendor.controller.js
@@ -0,0 +1,103 @@
+const vendorDb = require("../db/vendor.db");
+
+exports.create = async (req, res, next) => {
+  if (!req.body.name) {
+    res.status(400).send({
+      message: "Content cannot be empty!",
+    });
+    return;
+  }
+  const result = await vendorDb.create(req.body);
+
+  if (!result.succeeded) {
+    next(result.message);
+    return;
+  }
+  if (result.succeeded && result.message && result.message.length !== 0) {
+    res.status(200).send({
+      message: result.message,
+    });
+  } else res.status(201).send();
+};
+
+exports.findAll = async (req, res, next) => {
+  const result = await vendorDb.findAll();
+
+  if (!result.succeeded) {
+    next(result.message);
+    return;
+  }
+  if (result.succeeded && result.model) {
+    res.status(200).send({
+      model: result.model,
+    });
+  }
+};
+
+exports.findOne = async (req, res, next) => {
+  if (!req.params.id) {
+    res.status(400).send({
+      message: "Content cannot be empty!",
+    });
+    return;
+  }
+  const result = await vendorDb.findOne(req.params.id);
+
+  if (!result.succeeded) {
+    next(result.message);
+    return;
+  }
+
+  if (result.succeeded && result.message && result.message.length !== 0) {
+    res.status(200).send({
+      message: result.message,
+    });
+  } else {
+    res.status(200).send({
+      model: result.model,
+    });
+  }
+};
+
+exports.update = async (req, res, next) => {
+  if (!req.params.id || !req.body.name) {
+    res.status(400).send({
+      message: "Content cannot be empty!",
+    });
+    return;
+  }
+
+  const result = await vendorDb.update(req.params.id, { id: req.params.id, name: req.body.name });
+  if (!result.succeeded) {
+    next(result.message);
+    return;
+  }
+
+  if (result.succeeded && result.message && result.message.length !== 0) {
+    res.status(200).send({
+      message: result.message,
+    });
+  } else res.status(200).send();
+};
+
+exports.delete = async (req, res, next) => {
+  if (!req.params.id) {
+    res.status(400).send({
+      message: "Content cannot be empty!",
+    });
+    return;
+  }
+
+  const result = await vendorDb.delete(req.params.id);
+
+  if (!result.succeeded) {
+    next(result.message);
+    return;
+  }
+
+  if (result.succeeded && result.message && result.message.length !== 0) {
+    res.status(200).send({
+      message: result.message,
+    });
+  } else res.status(200).send();
+};
