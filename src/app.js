0a4639f6301a24494290ed9ce9a655a46bed0983
Egor Yudin <eydnep2000@gmail.com>
2020-10-19 21:53:29 +0300

    Refactor.

--- src/app.js
+++ src/app.js
@@ -1,24 +1,23 @@
+require("dotenv").config();
+
 const express = require("express");
-const middleware = require("./middlewares/index");
-const routes = require("./routes/index");
 
-const bodyParser = require("body-parser");
-const cors = require("cors");
-
-const corsOptions = {
-  origin: "http://localhost:8081"
-};
+const middleware = require("./middlewares");
+const routes = require("./routes");
 
 const app = express();
-const PORT = process.env.PORT || 4000;
-
-app.use(cors(corsOptions));
-
-app.use(bodyParser.json());
+const PORT = process.env.app_port;
+const { initializeDb } = require("./db");
 
 app.use("/", middleware);
 app.use("/", routes);
 
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
-});
+async function initialize() {
+  await initializeDb();
+
+  app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+  });
+}
+
+initialize();
