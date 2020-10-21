e6455d2cb114a96dc481226168a018a12718f7ea
Egor Yudin <eydnep2000@gmail.com>
2020-10-21 21:31:41 +0300

    Body and parameters validation for product and vendor was added.

--- src/routes/vendor.routes.js
+++ src/routes/vendor.routes.js
@@ -1,21 +1,32 @@
 const express = require("express");
+const { body, param } = require("express-validator");
+
 const vendors = require("../controllers/vendor.controller.js");
 
 const router = express.Router();
 
 // Create a new vendor
-router.post("/", vendors.create);
+router.post("/", [
+  body("name").isLength({ min: 2, max: 255 }),
+], vendors.create);
 
 // Retrieve all vendors
 router.get("/", vendors.findAll);
 
 // Retrieve a single vendor by id
-router.get("/:id", vendors.findOne);
+router.get("/:id", [
+  param("id").isInt({ gt: 0 }),
+], vendors.findOne);
 
 // Update a vendor by id
-router.put("/:id", vendors.update);
+router.put("/:id", [
+  param("id").isInt({ gt: 0 }),
+  body("name").isLength({ min: 2, max: 255 }),
+], vendors.update);
 
 // Delete a vendor by id
-router.delete("/:id", vendors.delete);
+router.delete("/:id", [
+  param("id").isInt({ gt: 0 }),
+], vendors.delete);
 
 module.exports = router;
