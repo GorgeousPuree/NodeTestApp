4d0f073fb62ff27007d9d0622216f3e4b4768999
Egor Yudin <eydnep2000@gmail.com>
2020-10-21 21:30:21 +0300

    Vendor controller refactor.

--- src/controllers/vendor.controller.js
+++ src/controllers/vendor.controller.js
@@ -1,103 +1,71 @@
+const { validationResult } = require("express-validator");
+
 const vendorDb = require("../db/vendor.db");
 
 exports.create = async (req, res, next) => {
-  if (!req.body.name) {
-    res.status(400).send({
-      message: "Content cannot be empty!",
-    });
-    return;
-  }
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
+
   const result = await vendorDb.create(req.body);
 
-  if (!result.succeeded) {
-    next(result.message);
-    return;
-  }
+  if (!result.succeeded) return next(result.message);
+
   if (result.succeeded && result.message && result.message.length !== 0) {
-    res.status(200).send({
-      message: result.message,
-    });
-  } else res.status(201).send();
+    return res.status(200).send({ message: result.message });
+  }
+
+  return res.status(201).send();
 };
 
 exports.findAll = async (req, res, next) => {
   const result = await vendorDb.findAll();
 
-  if (!result.succeeded) {
-    next(result.message);
-    return;
-  }
-  if (result.succeeded && result.model) {
-    res.status(200).send({
-      model: result.model,
-    });
-  }
+  if (!result.succeeded) return next(result.message);
+
+  return res.status(200).send({ model: result.model });
 };
 
 exports.findOne = async (req, res, next) => {
-  if (!req.params.id) {
-    res.status(400).send({
-      message: "Content cannot be empty!",
-    });
-    return;
-  }
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
+
   const result = await vendorDb.findOne(req.params.id);
 
-  if (!result.succeeded) {
-    next(result.message);
-    return;
-  }
+  if (!result.succeeded) return next(result.message);
 
   if (result.succeeded && result.message && result.message.length !== 0) {
-    res.status(200).send({
-      message: result.message,
-    });
-  } else {
-    res.status(200).send({
-      model: result.model,
-    });
+    return res.status(200).send({ message: result.message });
   }
+
+  return res.status(200).send({ model: result.model });
 };
 
 exports.update = async (req, res, next) => {
-  if (!req.params.id || !req.body.name) {
-    res.status(400).send({
-      message: "Content cannot be empty!",
-    });
-    return;
-  }
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
 
   const result = await vendorDb.update(req.params.id, { id: req.params.id, name: req.body.name });
-  if (!result.succeeded) {
-    next(result.message);
-    return;
-  }
+
+  if (!result.succeeded) return next(result.message);
 
   if (result.succeeded && result.message && result.message.length !== 0) {
-    res.status(200).send({
-      message: result.message,
-    });
-  } else res.status(200).send();
+    return res.status(200).send({ message: result.message });
+  }
+
+  return res.status(200).send();
 };
 
 exports.delete = async (req, res, next) => {
-  if (!req.params.id) {
-    res.status(400).send({
-      message: "Content cannot be empty!",
-    });
-    return;
-  }
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
 
   const result = await vendorDb.delete(req.params.id);
 
-  if (!result.succeeded) {
-    next(result.message);
-    return;
-  }
+  if (!result.succeeded) return next(result.message);
 
   if (result.succeeded && result.message && result.message.length !== 0) {
-    res.status(200).send({
-      message: result.message,
-    });
-  } else res.status(200).send();
+    return res.status(200).send({ message: result.message });
+  }
+
+  return res.status(200).send();
 };
