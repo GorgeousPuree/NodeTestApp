fc359597633c640a52af221088837b2b69fabd04
Egor Yudin <eydnep2000@gmail.com>
2020-10-21 21:13:38 +0300

    Small changes.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,19 @@
 module.exports = {
-    "env": {
-        "node": true,
-        "es6": true
+  env: {
+    node: true,
+    es6: true,
+  },
+  extends: "airbnb",
+  parserOptions: {
+    ecmaVersion: 11,
+  },
+  rules: {
+    "no-console": "off",
+    quotes: ["warn", "double"],
+  },
+  settings: {
+    react: {
+      version: "999.999.999",
     },
-    "extends": "airbnb",
-    "parserOptions": {
-        "ecmaVersion": 11
-    },
-    "rules": {
-        "no-console": "off",
-        "quotes": ["warn", "double"]
-    }
+  },
 };
--- src/app.js
+++ src/app.js
@@ -2,14 +2,14 @@ require("dotenv").config();
 
 const express = require("express");
 
-const middleware = require("./middlewares");
+const middlewares = require("./middlewares");
 const routes = require("./routes");
 
 const app = express();
 const PORT = process.env.app_port;
 const { initializeDb } = require("./db");
 
-app.use("/", middleware);
+app.use("/", middlewares);
 app.use("/", routes);
 
 async function initialize() {
