033db949d7cb1119b86254ba0c6b0e466ea6a91d
Egor Yudin <eydnep2000@gmail.com>
2020-10-21 21:31:41 +0300

    Body and parameters validation for product and vendor was added.

--- src/routes/category.routes.js
+++ src/routes/category.routes.js
@@ -1,21 +1,32 @@
 const express = require("express");
+const { body, param } = require("express-validator");
+
 const categories = require("../controllers/category.controller.js");
 
 const router = express.Router();
 
 // Create a new category
-router.post("/", categories.create);
+router.post("/", [
+  body("name").isLength({ min: 2, max: 255 }),
+], categories.create);
 
 // Retrieve all categories
 router.get("/", categories.findAll);
 
 // Retrieve a single category by id
-router.get("/:id", categories.findOne);
+router.get("/:id", [
+  param("id").isInt({ gt: 0 }),
+], categories.findOne);
 
 // Update a category by id
-router.put("/:id", categories.update);
+router.put("/:id", [
+  param("id").isInt({ gt: 0 }),
+  body("name").isLength({ min: 2, max: 255 }),
+], categories.update);
 
 // Delete a category by id
-router.delete("/:id", categories.delete);
+router.delete("/:id", [
+  param("id").isInt({ gt: 0 }),
+], categories.delete);
 
 module.exports = router;
