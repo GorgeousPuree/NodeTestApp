49fe3924c04ab4e463c910ac7c3ff1b7762be64a
Egor Yudin <eydnep2000@gmail.com>
2020-10-21 21:13:38 +0300

    Small changes.

--- src/middlewares/error.js
+++ src/middlewares/error.js
@@ -1,3 +1,4 @@
-module.exports.errorHandler = (message, req, res) => {
-  res.status(500).send({ message });
+// eslint-disable-next-line no-unused-vars
+module.exports.errorHandler = (err, req, res, next) => {
+  res.status(500).send({ message: err });
 };
