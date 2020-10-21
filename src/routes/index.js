9cf1be2bb1db5c0184ef95fdd7fa659b1c0ce05a
Egor Yudin <eydnep2000@gmail.com>
2020-10-21 21:23:38 +0300

    Routes for product were written.

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,13 @@ const { errorHandler } = require("../middlewares/error");
 
 const vendorRoutes = require("./vendor.routes");
 const categoryRoutes = require("./category.routes");
+const productRoutes = require("./product.routes");
 
 const router = express.Router();
 
 router.use("/vendor", vendorRoutes);
 router.use("/category", categoryRoutes);
+router.use("/product", productRoutes);
 router.use(errorHandler);
 
 module.exports = router;
